02c63a72eb09e96495ac9b67dc06f0b634c4fbfd
Deivid Oliveira <deividbenner17@gmail.com>
2022-11-22 20:56:58 -0300

    Estrutura mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AV1_TecPrograma_Uninove.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
